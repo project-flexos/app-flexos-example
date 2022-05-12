46a2662c1f86c14a7bf7cc04afe5157b0bf3e05b
Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>
2022-05-12 11:25:36 +0200

    prepare workshop simplified example
    Signed-off-by: Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright (c) 2020-2021, Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>
+ * Copyright (c) 2020-2022, Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,110 +34,44 @@
 #include <uk/alloc.h>
 #include <stdlib.h>
 
-#define EXIT_EARLY 0
+/* this is a temporary header necessary for EPT, should disappear
+ * in the future */
+#if CONFIG_LIBFLEXOS_VMEPT
+#include <flexos/impl/main_annotation.h>
+#endif
+
 
 /* In the future we want to have all flexos_gate and flexos_gate_r inserted
  * automatically; it will only be a matter of adding flexos_whitelist compiler
- * annotations and this can be automated via static analysis too (conservatively
- * of course, but seems to behave OK in practice, see Cali).
+ * annotations and this can be automated via static analysis too.
  */
 
-void check_success(int ret)
+/* a callback called by the library */
+void callback(int foo)
 {
-	if (!ret) {
-		flexos_gate(libc, printf, "SUCCESS!\n");
-	} else {
-		flexos_gate(libc, printf, "FAILURE\n");
-		exit(ret);
-	}
+	printf("callback called!\n");
 }
 
-/* static buffer that we pass to function1: have to be shared */
-static char static_buf[32]  __attribute__((flexos_whitelist));
-static char static_buf2[32] __attribute__((flexos_whitelist));
+/* static buffer that we pass to the library */
+static char static_buf[32];
 
-/* a private static buffer: only accessible from this compartment */
+/* a private static buffer */
 static char static_app_secret[32];
 
 int main(int __unused argc, char __unused *argv[])
 {
-	int ret;
-
 	static_buf[0] =  'A';
-	static_buf2[0] = 'B';
+	static_app_secret[0] = 'B';
 
-	/* shared stack integer: just as an example */
-	int stack_int __attribute__((flexos_whitelist)) = 21;
-	int stack_int2 __attribute__((flexos_whitelist)) = 42;
-	int stack_int3 __attribute__((flexos_whitelist));
-	stack_int3 = 84;
-	int stack_int4 __attribute__((flexos_whitelist));
-	stack_int4 = 168;
+	/* shared stack integer used by the library */
+	int stack_int = 21;
 
-	/* stack buffers that we pass to function1: have to be shared */
-	char stack_buf[32] __attribute__((flexos_whitelist));
+	/* shared stack buffer that we pass to the library */
+	char stack_buf[32];
 	stack_buf[0] = 'C';
 
-	/* this second definition makes sure that our transformations can be applied
-	 * multiple times in the same function. */
-	char stack_buf2[32] __attribute__((flexos_whitelist));
-	stack_buf2[0] = 'D';
+	/* call library function */
+	lib_func(&callback, static_buf, &stack_int, stack_buf);
 
-	/* heap buffers that we pass to function1: have to be shared */
-	char *heap_buf = flexos_malloc_whitelist(32, libflexosexample);
-	heap_buf[0] = 'E';
-
-	char *heap_buf2 = flexos_malloc_whitelist(32, libflexosexample);
-	heap_buf2[0] = 'F';
-
-	static_app_secret[20] = 'z';
-
-	/* This call to return is here just to test that stack-to-heap
-	 * transformations also add free() calls in mid-function return calls. */
-	if (EXIT_EARLY) {
-		return 0;
-	}
-
-	/* a private stack buffer: only accessible from this compartment */
-	static char stack_app_secret[32];
-	stack_app_secret[10] = static_app_secret[20];
-
-	/* a private heap buffer: only accessible from this compartment */
-	char *heap_app_secret = uk_malloc(uk_alloc_get_default(), 32);
-	heap_app_secret[10] = stack_app_secret[10];
-
-	flexos_gate(libc, printf, "Cross-compartment call with *statically* allocated shared data: ");
-	flexos_gate_r(libflexosexample, ret, function1, static_buf);
-	flexos_gate_r(libflexosexample, ret, function1, static_buf2);
-	/* note: no gates for check_success, this is a micro-lib internal call */
-	check_success(ret);
-
-	flexos_gate(libc, printf, "Cross-compartment call with *stack* allocated shared data: ");
-	flexos_gate_r(libflexosexample, ret, function1, stack_buf);
-	flexos_gate_r(libflexosexample, ret, function1, stack_buf2);
-	check_success(ret);
-
-	flexos_gate(libc, printf, "Cross-compartment call with *heap* allocated shared data: ");
-	flexos_gate_r(libflexosexample, ret, function1, heap_buf);
-	flexos_gate_r(libflexosexample, ret, function1, heap_buf2);
-	check_success(ret);
-
-	flexos_gate(libc, printf, "Printing my shared stack ints: %d %d %d %d\n",
-		stack_int, stack_int2, stack_int3, stack_int4);
-
-	flexos_gate(libc, printf, "Now executing an invalid cross-compartment access: THIS SHOULD CRASH NOW.\n");
-	/* static_app_secret should not be accessible from the other domain */
-	flexos_gate_r(libflexosexample, ret, function1, static_app_secret);
-
-	/* same as before */
-	if (EXIT_EARLY) {
-		return 0;
-	}
-
-	flexos_gate(libc, printf, "************* BUG *************");
-	flexos_gate(libc, printf, "We should have crashed already!");
-	flexos_gate(libc, printf, "*******************************");
-
-	/* no return call, but free() calls should still be inserted if
-	 * stack-to-heap conversion is enabled. */
+	return 0;
 }
